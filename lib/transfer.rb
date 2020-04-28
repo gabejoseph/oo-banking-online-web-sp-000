e3df3ffe1a0ec85b3793a9547f04e18772fb5091
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-28 17:59:59 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
       sender.balance -= amount
       @status = "complete"
     else 
-      binding.pry
+      # binding.pry
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
