16a5f5b2df4730c0fefceebed37f7421b0a40870
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-28 17:20:42 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   end 
 
   def execute_transaction 
-    if @amount > 0 && @status == "pending" && receiver.valid? && sender.valid?
+    if @amount > 0 && @status == "pending" 
       receiver.balance = receiver.balance + amount
       sender.balance = sender.balance - amount
       @amount = amount - amount
