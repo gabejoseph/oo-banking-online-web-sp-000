b0fc37c0a633b039f88af6b9930df4adf546235b
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-28 17:15:00 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,33 @@
+require 'pry'
 class BankAccount
+
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize(name)
+    @name = name 
+    @balance = 1000
+    @status = "open"
+  end 
+  
+  def deposit(deposit)
+    self.balance = @balance + deposit
+  end 
+  
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    if @status == "open" && @balance > 0 
+      return true
+    else 
+      return false
+    end 
+  end
+  
+  def close_account
+    self.status = "closed"
+  end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,33 @@
+require 'pry'
 class Transfer
-  # your code here
+  
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = 50
+  end
+  
+  def valid?
+    @sender.valid?
+    @receiver.valid?
+  end 
+
+  def execute_transaction 
+    if @amount > 0 && @status = "pending"
+      receiver.balance = receiver.balance + amount
+      sender.balance = sender.balance - amount
+      @amount = amount - amount
+      @status = "complete"
+    else 
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
+
+
+
+
 end
